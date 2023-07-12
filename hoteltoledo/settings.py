7c70e9558c960deaea2eee149fbb01af4421d84e
Gerlin Gonzalez <sadonfa@gmail.com>
2023-07-12 08:40:13 -0500

    Requerimientos

--- hoteltoledo/settings.py
+++ hoteltoledo/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'hotel_toledo.urls'
+ROOT_URLCONF = 'hoteltoledo.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'hotel_toledo.wsgi.application'
+WSGI_APPLICATION = 'hoteltoledo.wsgi.application'
 
 
 # Database
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_toledo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoteltoledo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
