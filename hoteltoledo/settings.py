b7dddda5a995f4bf183315c1f287b464db17e125
Gerlin Gonzalez <sadonfa@gmail.com>
2025-09-05 07:19:34 -0500

    base de datos sqlite

--- hoteltoledo/settings.py
+++ hoteltoledo/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'hoteltoledo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE':  'django.db.backends.mysql',
-        'NAME': env('POSTGRESQL_NAME'),
-        'USER': env('POSTGRESQL_USER'),
-        'PASSWORD': env('POSTGRESQL_PASS'),
-        'HOST': env('POSTGRESQL_HOST'),
-        'PORT': env('POSTGRESQL_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE':  'django.db.backends.mysql',
+#         'NAME': env('POSTGRESQL_NAME'),
+#         'USER': env('POSTGRESQL_USER'),
+#         'PASSWORD': env('POSTGRESQL_PASS'),
+#         'HOST': env('POSTGRESQL_HOST'),
+#         'PORT': env('POSTGRESQL_PORT'),
+#     }
+# }
 
 
 # Password validation
